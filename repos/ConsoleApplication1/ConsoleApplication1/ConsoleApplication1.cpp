4d00c6f4b4ca659a2bfee2ee17103ffa03d6483f
moha327 <m7amad9595@outlook.com>
2024-07-17 19:57:09 +0300

    cpp

---
+++ repos/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+void displaycardinfo() {
+	cout << "********************************************************\n";
+	cout << "Name: " << "Mohammed Hammouz\n";
+	cout << "Age: " << 29 << "\n";
+	cout << "City: " << "Nablus\n" ;
+	cout << "Country: " << "Palestine\n";
+	cout << "********************************************************"<<endl;
+}
+int main(){
+	displaycardinfo();
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
