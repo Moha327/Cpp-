f7162b183c483aa08421730135b013d974d5b007
moha327 <m7amad9595@outlook.com>
2024-07-31 11:49:25 +0300

    cpp

--- repos/sumof_three_numbers/sumof_three_numbers/sumof_three_numbers.cpp
+++ repos/sumof_three_numbers/sumof_three_numbers/sumof_three_numbers.cpp
@@ -2,19 +2,57 @@
 //
 
 #include <iostream>
+#include <string>
 using namespace std;
+//struct strnumbers {
+//    unsigned short int num1;
+//    unsigned short int num2;
+//    unsigned short int num3;
+//};
+//
+//strnumbers Read_numbers() {
+//    strnumbers Number;
+//    cout << "enter num1" << endl;
+//    cin >> Number.num1;
+//    cout << "enter num2" << endl;
+//    cin >> Number.num2;
+//    cout << "enter num3" << endl;
+//    cin >> Number.num3;
+//    return Number;
+//}
+//string Calculate_sum_of_numbers(strnumbers Number) {
+//    string Result;
+//    Result = "sum of three numbers is " + to_string(Number.num1 + Number.num2 + Number.num3);
+//    return Result;
+//}
+//
+//void Print_Sum(strnumbers Number) {
+//    cout << Calculate_sum_of_numbers(Number) << endl;
+//}
+
+unsigned short int Read_Numbers(unsigned short int& num1, unsigned short int& num2, unsigned short int& num3) {
+    cout << "enter num1" << endl;
+        cin >> num1;
+        cout << "enter num2" << endl;
+        cin >> num2;
+        cout << "enter num3" << endl;
+        cin >> num3;
+        return 0;
+}
+unsigned short int Calculate_sum_of_numbers(unsigned short int num1, unsigned short int num2, unsigned short int num3) {
+    return num1 + num2 + num3;
+}
+
+void Print_Sum(unsigned short int Result) {
+       cout << Result << endl;
+}
 int main()
 {
-    long long int num1;
-    long long int num2;
-    long long int num3;
-    cout << "enter num1" << endl;
-    cin >> num1;
-    cout << "enter num2" << endl;
-    cin >> num2;
-    cout << "enter num3" << endl;
-    cin >> num3;
-    cout << "sum of three numbers is " << num1+num2+num3<< endl;
+    unsigned short int num1;
+    unsigned short int num2;
+    unsigned short int num3;
+    Read_Numbers(num1, num2, num3);
+    Print_Sum(Calculate_sum_of_numbers( num1, num2, num3));
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
