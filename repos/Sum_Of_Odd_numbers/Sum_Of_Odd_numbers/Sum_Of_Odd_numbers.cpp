9a8a5886e7ce8c8dbd66eb56d790e7acb8fbd296
moha327 <m7amad9595@outlook.com>
2024-08-04 14:10:50 +0300

    cpp

--- repos/Sum_Of_Odd_numbers/Sum_Of_Odd_numbers/Sum_Of_Odd_numbers.cpp
+++ repos/Sum_Of_Odd_numbers/Sum_Of_Odd_numbers/Sum_Of_Odd_numbers.cpp
@@ -2,31 +2,47 @@
 //
 #include <iostream>
 using namespace std;
-void ReadNumber(int& num) {
-    cout << "Enter num from 1 to 100 please:\n";
-    cin >> num;
-    if (num < 1 || num>100) {
-        ReadNumber(num);
-    }
+//void ReadNumber(int& num) {
+//    cout << "Enter num from 1 to 100 please:\n";
+//    cin >> num;
+//    if (num < 1 || num>100) {
+//        ReadNumber(num);
+//    }
+//}
+//
+//int PrintSumNum(int num) {
+//    int sum = 0;
+//    for (int i = 1; i <= num; i++) {
+//        if (i % 2 == 1) {
+//            sum += i;
+//        }
+//    }
+//    return sum;
+//}
+unsigned short int Read_Number() {
+    unsigned short int N;
+    cout << "Pleae enter number that you want:\n";
+    cin >> N;
+    return N;
 }
-
-int PrintSumNum(int num) {
-    int sum = 0;
-    for (int i = 1; i <= num; i++) {
-        if (i % 2 == 1) {
+unsigned short int Find_Odd_Numbers(unsigned short int N) {
+    unsigned short int sum=0;
+    for (unsigned short int i = 1; i < N + 1; i++) {
+        if (i % 2 == 1)
             sum += i;
-        }
     }
     return sum;
 }
-
+void Print_Sum(unsigned short int sum) {
+    cout << "Sum of even numbers is: " << sum << endl;
+}
 int main()
 {
-    int num;
+    /*int num;
     ReadNumber(num);
     cout << PrintSumNum(num) << endl;
-    return 0;
-
+    return 0;*/
+    Print_Sum(Find_Odd_Numbers(Read_Number()));
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
