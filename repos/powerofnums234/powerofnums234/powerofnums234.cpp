c8954754ced29f14505fcfd71d29f75949e4a095
moha327 <m7amad9595@outlook.com>
2024-08-04 14:39:51 +0300

    cp

--- repos/powerofnums234/powerofnums234/powerofnums234.cpp
+++ repos/powerofnums234/powerofnums234/powerofnums234.cpp
@@ -3,23 +3,54 @@
 
 #include <iostream>
 #include <cmath>
-#include <iomanip>
 using namespace std;
 
-void powerofnum234procedure(long double num) {
-    cout << fixed;
-    cout << "The pwoer 2 of number is: " << pow(num,2) << "\n";
-    cout << "The pwoer 3 of number is: " << pow(num, 3) << "\n";
-    cout << "The pwoer 4 of number is: " << pow(num, 4) << "\n";
-    cout << "The round of pwoer 2 of number is: " << round(pow(num, 2)) << endl;
-    cout << "The round of pwoer 3 of number is: " << round(pow(num, 3)) << "\n";
-    cout << "The round of pwoer 4 of number is: " << round(pow(num, 4)) << "\n";
-
+void Read_Number(int& N) {
+    cout << "Pleae enter number that you want:\n";
+    cin >> N;
 }
+int Find_Power_2_Of_Number(int N) {
+    int power2;
+    power2 = pow(N, 2);
+    return power2;
+}
+int Find_Power_3_Of_Number(int N) {
+    int power3;
+    power3 = pow(N, 3);
+    return power3;
+}
+int Find_Power_4_Of_Number(int N) {
+    int power4;
+    power4 = pow(N, 4);
+    return power4;
+}
+void Print_Pow2(int N) {
+    int power2 = N;
+    cout << "Power 2 of number is: " << power2 << "\n";
+}
+void Print_Pow3(int N) {
+    int power3 = N;
+    cout << "Power 3 of number is: " << power3 << "\n";
+}
+void Print_Pow4(int N) {
+    int power4 = N;
+    cout << "Power 4 of number is: " << power4 << endl;
+}
+
+//void powerofnum234procedure(long double num) {
+//    cout << fixed;
+//    cout << "The pwoer 2 of number is: " << pow(num,2) << "\n";
+//    cout << "The pwoer 3 of number is: " << pow(num, 3) << "\n";
+//    cout << "The pwoer 4 of number is: " << pow(num, 4) << "\n";
+//    cout << "The round of pwoer 2 of number is: " << round(pow(num, 2)) << endl;
+//    cout << "The round of pwoer 3 of number is: " << round(pow(num, 3)) << "\n";
+//    cout << "The round of pwoer 4 of number is: " << round(pow(num, 4)) << "\n";
+//
+//}
 
 int main()
 {
-    long double num;
+    /*long double num;
     cout << fixed;
     cout << "Enter number please:\n";
     cin >> num;
@@ -29,7 +60,12 @@ int main()
     cout << "The round of pwoer 2 of number is: " << round(num * num) << "\n";
     cout << "The round of pwoer 3 of number is: " << round(num * num * num) << "\n";
     cout << "The round of pwoer 4 of number is: " << round(num * num * num * num) << endl;
-    powerofnum234procedure(num);
+    powerofnum234procedure(num);*/
+    int N;
+    Read_Number(N);
+    Print_Pow2(Find_Power_2_Of_Number(N));
+    Print_Pow3(Find_Power_3_Of_Number(N));
+    Print_Pow4(Find_Power_4_Of_Number(N));
     return 0;
 }
 
